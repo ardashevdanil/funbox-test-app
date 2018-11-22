fdf0ca5593d466a21fcd42dda10b149dd5bc38e4
Ardashev Danil <ardashev_danil@yahoo.com>
2018-11-23 01:19:22 +0300

    added some material-ui components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import MyMap from '../MyMap';
 import PointInput from '../PointInput';
 import PointsList from '../PointsList';
-import './App.css';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  min-width: 640px;
+  display: flex;
+
+  & > div {
+    width: 50%;
+    height: calc(100% -20px);
+    margin 10px;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+`
 
 const App = () => (
-  <div className='App'>
+  <Wrapper>
     <div className='points-menu'>
       <PointInput />
       <PointsList />
     </div>
     <MyMap />
-  </div>
+  </Wrapper>
 );
 
 export default App;
