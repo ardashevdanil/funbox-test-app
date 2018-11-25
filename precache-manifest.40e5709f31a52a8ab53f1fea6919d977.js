268822063aef29eecab21e6d834cd48a0e305eff
Ardashev Danil <ardashev_danil@yahoo.com>
2018-11-25 20:25:01 +0300

    Updates

---
+++ precache-manifest.40e5709f31a52a8ab53f1fea6919d977.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a84429aa108b21b155b1",
+    "url": "/funbox-test-app/static/js/runtime~main.a84429aa.js"
+  },
+  {
+    "revision": "3367bda30b8eed37ab88",
+    "url": "/funbox-test-app/static/js/main.3367bda3.chunk.js"
+  },
+  {
+    "revision": "b418685861722299804f",
+    "url": "/funbox-test-app/static/js/1.b4186858.chunk.js"
+  },
+  {
+    "revision": "b726671eb6aee1dcba57d935c6bfca94",
+    "url": "/funbox-test-app/index.html"
+  }
+];
